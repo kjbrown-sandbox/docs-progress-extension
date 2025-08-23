38a82f5eba714ea1eafffbca295b3860a7e6bc8a
Kimberly Brown <kjbrown.home@gmail.com>
2025-08-23 15:52:05 -0600

    Add a sidepanel that pops up when you click the icon

--- src/popup/main.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Popup from "./Popup";
-
-const root = createRoot(document.getElementById("root")!);
-root.render(<Popup />);
